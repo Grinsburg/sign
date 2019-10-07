e7e6cb664523bcdcfb2a9a2e46743eda6d08cedb
Grinsburg <smlynchik@mail.ru>
2019-10-08 00:30:57 +0300

    deployment to GHP

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
+
 import Sign from '../Sign/Sign'
 
 import {Container, Row} from 'react-bootstrap'
 
 function App() {
   return (
-          <Container>
-              <Row className="d-flex align-items-center" style={{height: "100vh"}}>
-                  <Sign />
-              </Row>
-          </Container>
+      <Container>
+          <Row className="d-flex align-items-center" style={{height: "100vh"}}>
+              <Sign />
+          </Row>
+      </Container>
   );
 }
 
