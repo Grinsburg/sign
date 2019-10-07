c21c6710358a312cabf9e936c5807925cb160af3
Grinsburg <smlynchik@mail.ru>
2019-10-08 00:28:03 +0300

    Updates

---
+++ precache-manifest.60b07c1f071d8e666f082d0fcaeff236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab92bacad2a532c88c0f445b3cf67fdc",
+    "url": "/sign/index.html"
+  },
+  {
+    "revision": "0fbd720e685a302cab81",
+    "url": "/sign/static/css/main.7254ea07.chunk.css"
+  },
+  {
+    "revision": "7d08e666f0ad18b63a53",
+    "url": "/sign/static/js/2.4ccc4f40.chunk.js"
+  },
+  {
+    "revision": "0fbd720e685a302cab81",
+    "url": "/sign/static/js/main.b69f0d07.chunk.js"
+  },
+  {
+    "revision": "2de90f91f9ff092ece2b",
+    "url": "/sign/static/js/runtime-main.b714ece1.js"
+  },
+  {
+    "revision": "25ce5a3ee3a01d7fcc62b8e28b30af7b",
+    "url": "/sign/static/media/img.25ce5a3e.png"
+  }
+]);
